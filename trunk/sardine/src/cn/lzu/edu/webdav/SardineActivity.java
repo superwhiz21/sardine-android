b4f0f3d9925a4de023d3a992673f8c2599f44f20
sunyanmeng@gmail.com <sunyanmeng@gmail.com@1b7e1a73-b120-31e4-0d44-ead8919cc617>
2012-01-11 06:03:13 +0000

    修改程序图标，程序基本功能基本完成 待解决BUG：不能递归下载、复制、移动，不能对包含汉字的文件进行复制移动，没有对文件列表进行基本的排序

--- trunk/sardine/src/cn/lzu/edu/webdav/SardineActivity.java
+++ trunk/sardine/src/cn/lzu/edu/webdav/SardineActivity.java
@@ -1,36 +1,28 @@
 package cn.lzu.edu.webdav;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.simpleframework.xml.Serializer;
-import org.simpleframework.xml.core.Persister;
-
-import com.googlecode.sardine.DavResource;
-import com.googlecode.sardine.Sardine;
-import com.googlecode.sardine.SardineFactory;
-
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.AdapterView.OnItemClickListener;
 
 public class SardineActivity extends Activity {
-	private String ROOT = "http://202.201.1.135:30080/mnt/li/lzu1/s1/";
-	private String TAG = "Sardine";
+	
+	private static final int aboutMenu = Menu.FIRST;
+	private static final int exitMenu = Menu.FIRST + 1;
 	
 	private ListView mList;
 	
@@ -38,11 +30,12 @@ public class SardineActivity extends Activity {
 	
 	private List<String> mListData;
 
-	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		
+		this.setTitle(this.getResources().getString(R.string.app_full_name));
 		
 		mList = (ListView)findViewById(R.id.mNasList);
         initListData();
@@ -50,10 +43,43 @@ public class SardineActivity extends Activity {
         mAdapter = new ArrayAdapter<String>(this, R.layout.item, mListData);
         mList.setAdapter(mAdapter);
         mList.setOnItemClickListener(new ClickListListener());
-        setTitle("NasStorage Demo");
 	}
 	
-	 private void initListData(){
+	 @Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		 final Resources localResources = this.getResources();
+		 menu.add(Menu.NONE, aboutMenu, 1, localResources.getString(R.string.about)).setIcon(
+				 android.R.drawable.ic_menu_info_details);
+		 menu.add(Menu.NONE, exitMenu, 2, localResources.getString(R.string.exit)).setIcon(
+				 android.R.drawable.ic_menu_close_clear_cancel);
+		 return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case aboutMenu:
+			Intent Intent2 = new Intent();
+			Intent2.setClass(SardineActivity.this, AboutActivity.class);
+			startActivity(Intent2);
+			break;
+		case exitMenu:
+			dialog();
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+			dialog();
+			return false;
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+
+	private void initListData(){
 	    	Resources localResource = this.getResources();
 	    	mListData = new ArrayList<String>();
 	    	mListData.add(localResource.getString(R.string.nas_file));
@@ -64,14 +90,13 @@ public class SardineActivity extends Activity {
 
 			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
 					long arg3) {
-				// TODO Auto-generated method stub
 				Intent localIntent = new Intent();
 				switch(arg2){
 				case 0:
 					localIntent.setClass(SardineActivity.this, NasStorageNasList.class);
 					break;
 				case 1:
-//					localIntent.setClass(SardineActivity.this, NasStorageLocalList.class);
+					localIntent.setClass(SardineActivity.this, NasStorageLocalList.class);
 					break;
 				}
 				SardineActivity.this.startActivity(localIntent);
@@ -79,6 +104,30 @@ public class SardineActivity extends Activity {
 			}
 	    	
 	    }
+	    
+	    private void dialog() {
+	    	final Resources localResources = this.getResources();
+			AlertDialog.Builder builder = new Builder(SardineActivity.this);
+			builder.setMessage(localResources.getString(R.string.finish));
+			builder.setTitle(localResources.getString(R.string.prompt));
+			builder.setPositiveButton(localResources.getString(R.string.ok),
+					new android.content.DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							dialog.dismiss();
+							SardineActivity.this.finish();
+						}
+					});
+			builder.setNegativeButton(localResources.getString(R.string.cancel),
+					new android.content.DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							dialog.dismiss();
+						}
+					});
+			builder.create().show();
+
+		}
 }
 		/*Sardine sardine = SardineFactory.begin("lzu", "nopasswd");
 		List<DavResource> resources;
